b25bb0b840f2bba1a13fa1947b9e8aeb99ad5ab6
DenyAmanda <167731618+DenyAmanda@users.noreply.github.com>
2024-06-27 15:42:06 +0700

    Add files via upload

---
+++ Tampilpelanggan.php
@@ -0,0 +1,34 @@
+<table align="center" border="1">
+<tr>
+<td>Kode Pelanggan</td><td> Nama 
+Pelanggan</td><td>Jenis 
+Kelamin</td><td>Alamat</td><td>Nomor 
+Telepon</td>
+<td colspan="2">Keterangan</td>
+</tr>
+ <tr><td>1</td>
+ <td>Deny</td>
+ <td>Laki-laki</td>
+ <td>Kuala</td>
+ <td>083155783812</td>
+ <td><a 
+href=editpelanggan.php?kode_pelanggan=4001>E
+dit</td>
+ <td>
+<a 
+href=hapuspelanggan.php?kode_pelanggan=4001>Hap
+us</td>
+ <tr><tr><td>2</td>
+ <td>Melin</td>
+ <td>Perempuan</td>
+ <td>Selesai</td>
+ <td>085758663511</td>
+ <td><a 
+href=editpelanggan.php?kode_pelanggan=4002>Edit</
+td>
+ <td><a 
+href=hapuspelanggan.php?kode_pelanggan=4002>Hap
+us</td>
+ 
+ <tr><tr><td>Jumah 
+Barang</td><td>4</td></tr></table
---
+++ judul.php
@@ -0,0 +1,10 @@
+<html xmlns="https://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type"content="text/html; charse=utf-8"/>
+        <title>Untitled Document</title>
+</head>
+
+<body bgcolor="#00FFF">
+    <h2><center>Selamat Datang di Toko Online TI-a</center></h2>
+</body>
+</html>
